ceff055d7198e6c82508f8892a51fa3b8cd8fb24
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2024-09-04 13:48:00 +0200

    Up till Page 50 before Vectors

--- Introduction to Functions.R
+++ Introduction to Functions.R
@@ -32,3 +32,55 @@ return(k) #At the end of the code we use the return statement
 g <- function(x,y=2,z=t) {...} #y is initialized to 2 if not specified in call, similarly z is initialized to TRUE 
 
 g(12,z=FALSE) #12 is the actual argument for x, y remains 2 as initialized above but z is overwritten to FALSE
+
+#R Data Structures
+
+#VECTORS ELEMENTS OF A VECTOR MUST ALL HAVE THE SAME MODE/DATA TYPE
+#A vector CAN consist of 3 character strings (the mode/data type is character)
+#A vector CAN consist of 3 integer elements(the mode is integer)
+#A vector CANNOT consist of an integer element and a character string(2 differing modes)
+
+#Scalars are individual numbers but in R they appear as one-element vectors
+
+#Character strings are single element vectors of mode character
+ 
+z <- c("abc","29 88") #assigning z to concatenated data with 2 strings ("1st string","2nd string")
+length(z)  #length will output 2 as there are 2 strings
+mode(z)   #mode will output be character as data was written as string ""   
+
+#Matrices are vectors with 2 dimensional attributes
+
+m <- rbind(c(1,4),c(2,2)) #rbind() function is row bind combining rows to build a matrix
+m
+m %*% c(1,1) #%% is the matrix-manipulation operator
+
+#Lists are containers for values but the contents can be different data types
+#List elements are accessed using 2-part names indicated with the $ sign
+
+x <- list(u=2, v="abc") #In x I have assigned the list with numeric and charater  
+x
+
+#A common use of lists is to combine multiple values into a single package that can be returned by a function
+hist(Nile)        #Outputs a graph
+hn <- hist(Nile)   # hist(Nile) is assigned to hn
+hn           #Outputs hist(Nile) \\list of components describing the characteristics of the histogram
+str(hn) #str stands for structure 
+        #str outputs a more compact alternative to simply calling hn 
+
+#Data frames are lists with each component of the list being a vector corresponding to a column in our matrix of data
+d <- data.frame(list(kids=c("Jack","Jill"),ages=c(12,10)))
+d
+
+#Classes - Objects are instances of classes -their instances are simply lists but with an extra attribute : the class name
+
+print(hn)
+
+#Classes are used for generic functions
+#Generic functions are family of functions serving a similar purpose but each appropriate to a specific class
+#e.g. summary()
+summary(hn)
+plot()
+#Classes are used to organize objects
+
+#example() runs examples of help() entries
+example(persp) 
